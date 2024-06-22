5f8dce33b6ac8ffbcb0a67627bed4ac8d1518fdf
AlexPet <alexandrpet195@gmail.com>
2024-06-23 00:44:55 +0300

    need to fix Invalid src prop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,14 +6,32 @@ export default function Home() {
     <div className="flex flex-wrap justify-center">
       <div className="box-border h-64 w-64 p-4 border-2 mr-1 mb-1">
         <Images />
-        <Image src="/girl1.jpg" alt="photo" width={256} height={256} />
+        <Image
+          priority={false}
+          src="/girl1.jpg"
+          alt="photo"
+          width={256}
+          height={256}
+        />
         {/* <div>txt</div> */}
       </div>
       <div className="box-border h-64 w-64 p-4 border-2 mr-1 mb-1">
-        <Image src="/girl2.jpg" alt="photo" width={256} height={256} />
+        <Image
+          priority={false}
+          src="/girl2.jpg"
+          alt="photo"
+          width={256}
+          height={256}
+        />
       </div>
       <div className="box-border h-64 w-64 p-4 border-2 mr-1 mb-1">
-        <Image src="/girl3.jpg" alt="photo" width={256} height={256} />
+        <Image
+          priority={false}
+          src="/girl3.jpg"
+          alt="photo"
+          width={256}
+          height={256}
+        />
       </div>
     </div>
   );
--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -1,46 +1,44 @@
 import { put } from "@vercel/blob";
 import { revalidatePath } from "next/cache";
-import { Pool } from "@vercel/postgres";
-
-const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL,
-});
+import { sql } from "@vercel/postgres";
 
 export default function Form() {
   async function uploadImage(formData: FormData) {
     "use server";
     const imageFile = formData.get("image") as File;
-    const client = await pool.connect();
-    try {
-      const query = "INSERT INTO images (filename, data) VALUES ($1, $2)";
-      await client.query(query);
-    } catch {
-      client.release();
-    }
-    const blob = await put(imageFile.name, imageFile, {
+    const { url } = await put(imageFile.name, imageFile, {
       access: "public",
     });
+    try {
+      sql`INSERT INTO images (filename, url) VALUES (${imageFile.name}, ${url})`;
+      console.log("image add");
+    } catch (e) {
+      console.log(e);
+    }
     revalidatePath("/");
-    return Response.json({ blob });
+    return Response.json({ url });
   }
 
   return (
-    <label className="bg-black text-gray-500 font-semibold text-base rounded max-w-md h-52 flex flex-col items-center justify-center cursor-pointer border-2 border-gray-300 border-dashed mx-auto font-[sans-serif]">
-      <svg className="w-11 mb-2 fill-gray-500" viewBox="0 0 32 32">
-        <path
-          d="M23.75 11.044a7.99 7.99 0 0 0-15.5-.009A8 8 0 0 0 9 27h3a1 1 0 0 0 0-2H9a6 6 0 0 1-.035-12 1.038 1.038 0 0 0 1.1-.854 5.991 5.991 0 0 1 11.862 0A1.08 1.08 0 0 0 23 13a6 6 0 0 1 0 12h-3a1 1 0 0 0 0 2h3a8 8 0 0 0 .75-15.956z"
-          data-original="#000000"
-        />
-        <path
-          d="M20.293 19.707a1 1 0 0 0 1.414-1.414l-5-5a1 1 0 0 0-1.414 0l-5 5a1 1 0 0 0 1.414 1.414L15 16.414V29a1 1 0 0 0 2 0V16.414z"
-          data-original="#000000"
-        />
-      </svg>
-      Upload file
-      <input type="file" id="uploadFile1" className="hidden" />
-      <p className="text-xs font-medium text-gray-400 mt-2">
-        PNG, JPG, and SVG are Allowed.
-      </p>
-    </label>
+    <form action={uploadImage}>
+      <label className="bg-black text-gray-500 font-semibold text-base rounded max-w-md h-52 flex flex-col items-center justify-center cursor-pointer border-2 border-gray-300 border-dashed mx-auto font-[sans-serif]">
+        <svg className="w-11 mb-2 fill-gray-500" viewBox="0 0 32 32">
+          <path
+            d="M23.75 11.044a7.99 7.99 0 0 0-15.5-.009A8 8 0 0 0 9 27h3a1 1 0 0 0 0-2H9a6 6 0 0 1-.035-12 1.038 1.038 0 0 0 1.1-.854 5.991 5.991 0 0 1 11.862 0A1.08 1.08 0 0 0 23 13a6 6 0 0 1 0 12h-3a1 1 0 0 0 0 2h3a8 8 0 0 0 .75-15.956z"
+            data-original="#000000"
+          />
+          <path
+            d="M20.293 19.707a1 1 0 0 0 1.414-1.414l-5-5a1 1 0 0 0-1.414 0l-5 5a1 1 0 0 0 1.414 1.414L15 16.414V29a1 1 0 0 0 2 0V16.414z"
+            data-original="#000000"
+          />
+        </svg>
+        Upload file
+        <input type="file" name="image" id="uploadFile1" className="hidden" />
+        <p className="text-xs font-medium text-gray-400 mt-2">
+          PNG, JPG, and SVG are Allowed.
+        </p>
+      </label>
+      <button type="submit"> Submit </button>
+    </form>
   );
 }
