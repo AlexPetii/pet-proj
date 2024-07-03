65c3392b397609d53449b82f9e356b7c5da0dd34
AlexPet <alexandrpet195@gmail.com>
2024-07-04 01:40:56 +0300

    edit proj page

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -1,3 +1,11 @@
-export default function Home() {
-  return <div>Project Page</div>;
+"use client";
+
+import { useUser } from "@clerk/nextjs";
+
+export default function Project() {
+  const { isSignedIn, user } = useUser();
+  if (!isSignedIn) {
+    return null;
+  }
+  return <div>Hello, {user.username}</div>;
 }
